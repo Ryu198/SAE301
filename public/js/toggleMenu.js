d9e471cb3761a88de994d66ea16503a8fe28825e
aliceghanimi@hotmail.com <aliceghanimi@hotmail.com>
2023-12-03 12:39:56 +0000

    commit final : ajout de modifier/publier

--- public/js/toggleMenu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.menu-icon');
-    const sidebar = document.querySelector('.sidebar');
-
-    menuIcon.addEventListener('click', function() {
-        sidebar.classList.toggle('active');
-    });
-});
